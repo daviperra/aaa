d14cfb3c32e75bdc4c54ef8eb95309e93296f9b8
daviperra <140338092+daviperra@users.noreply.github.com>
2023-07-25 12:33:50 +0200

    Update special.c

--- special.c
+++ special.c
@@ -1,15 +1,38 @@
 #include "carte.h"
 
+/** La procedura duello gestisce la giocata della carta duello da parte di un giocatore.
+ * Il giocatore che gioca la carta duello sceglie un avversario da sfidare indipendentemente
+ * dalla distanza. Dopo aver scelto il giocatore da sfidare inizia il duello: i due giocatori
+ * scarteranno a turno un bang dalla propria mano fino a che uno dei due non avra' piu' carte bang
+ * disponibili. Il giocatore che perde il duello perde un punto vita e se si trova a zero punti vita
+ * potra' giocare una birra in extremis o essere eliminato.
+ *
+ * @param party party di giocatori
+ * @param nGiocatori numero di giocatori
+ * @param sfidante giocatore che usa duello
+ * @param mazzoScarti mazzo scarti
+ * @param mazzoPesca mazzo pesca per eventuali bonus
+ * @param nomeFile nome file log
+**/
+
 void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoScarti, Mazzo* mazzoPesca, char* nomeFile)
 {
     printf("\n\nStai giocando DUELLO.\n\n");
     continuo();
 
+    //scelta giocatore da sfidare
     for(int i=0; i<(*nGiocatori); i++)
     {
         if(!comparaNomi(sfidante->nome, party[i].nome))
         {
-            printf("%d - %s.\n", i, party[i].nome);
+            if(party[i].ruolo == SCERIFFO)
+            {
+                printf("%d - %s (SCERIFFO).", i, party[i].nome);
+            }
+            else
+            {
+                printf("%d - %s.\n", i, party[i].nome);
+            }
         }
     }
 
@@ -31,6 +54,7 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
     strcat(messaggio, " viene sfidato a duello.");
     salvaLog(messaggio, nomeFile);
 
+    getchar();
     printf("\n%s! %s ti sfida a duello!\n" , party[idBersaglio].nome, sfidante->nome);
     continuo();
     pulisciSchermo();
@@ -49,38 +73,32 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
 
         continuo();
 
-        while(i<party[idBersaglio].mano.numCarte && cartaBersaglio == NOP)
-        {
-            if(comparaNomi(party[idBersaglio].mano.carte[i].nome, BANG))
-            {
-                cartaBersaglio = i;
-            }
-            i++;
-        }
+        // cerco bang da scartare nella mano del bersaglio
+        cartaBersaglio = trovaCartaMano(&party[idBersaglio], BANG);
 
+        // se viene trovato un bang nella mano del bersaglio la sfida continua
         if(cartaBersaglio != NOP)
         {
             printf("\n%s gioca un BANG! e si difende. La sfida continua.\n", party[idBersaglio].nome);
+            spostaCarta(&party[idBersaglio].mano, cartaBersaglio, mazzoScarti);
+
             continuo();
             pulisciSchermo();
 
             printf("\nTocca a %s!\n", sfidante->nome);
             continuo();
 
-            while (j < sfidante->mano.numCarte && cartaSfidante == -1)
-            {
-                if(comparaNomi(sfidante->mano.carte[j].nome, BANG))
-                {
-                    cartaSfidante = j;
-                }
-                j++;
-            }
+            // cerco bang da scartare nella mano dello sfidante
+            cartaSfidante = trovaCartaMano(&party[idBersaglio], BANG);
 
+            // se si trova un bang nella mano dello sfidante la sfida continua
             if(cartaSfidante != NOP)
             {
                 printf("\n%s gioca un BANG! e si difende. La sfida continua.\n", sfidante->nome);
+                spostaCarta(&sfidante->mano, cartaSfidante, mazzoScarti);
                 continuo();
             }
+            // se non si trova un bang nella mano dello sfidante si aggiorna la variabile esito per terminare il duello
             else
             {
                 esito = 1;
@@ -88,6 +106,7 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
                 continuo();
             }
         }
+            // se non si trova un bang nella mano del bersaglio si aggiorna la variabile esito per terminare il duello
         else
         {
             esito = 0;
@@ -95,18 +114,12 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
             continuo();
         }
 
-        if(cartaBersaglio != NOP && cartaSfidante != NOP)
-        {
-            spostaCarta(&party[idBersaglio].mano, cartaBersaglio, mazzoScarti, NULL, nomeFile);
-            spostaCarta(&sfidante->mano, cartaBersaglio, mazzoScarti, NULL, nomeFile);
-        }
-
         pulisciSchermo();
 
     }while(esito == NOP);
 
     int idBirra = NOP;
-
+    // gestisco il danno recato al bersaglio
     if(esito == 0)
     {
         printf("\n%s! Sei stato colpito!\n", party[idBersaglio].nome);
@@ -122,7 +135,7 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
         {
             if (birraExtremis(&party[idBersaglio], nomeFile))
             {
-                spostaCarta(&sfidante->mano, idBirra, mazzoScarti, NULL, nomeFile);
+                spostaCarta(&sfidante->mano, idBirra, mazzoScarti);
                 birra(&party[idBersaglio], nomeFile);
             }
             else
@@ -131,6 +144,7 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
             }
         }
     }
+    // gestisco il danno arrecato al bersaglio
     else
     {
         printf("\n%s! Sei stato colpito!\n", sfidante->nome);
@@ -145,16 +159,30 @@ void duello(Giocatore* party,int* nGiocatori, Giocatore* sfidante, Mazzo* mazzoS
         {
             if (birraExtremis(sfidante, nomeFile))
             {
-                spostaCarta(&sfidante->mano, idBirra, mazzoScarti, NULL, nomeFile);
+                spostaCarta(&sfidante->mano, idBirra, mazzoScarti);
                 birra(sfidante, nomeFile);
             }
             else
             {
-                eliminazione(party, sfidante, &party[idBersaglio], nGiocatori, mazzoPesca, mazzoScarti, nomeFile);
+                eliminazione(party, sfidante, sfidante, nGiocatori, mazzoPesca, mazzoScarti, nomeFile);
             }
         }
     }
 }
+
+/** La procedura Indiani gestisce la giocata della carta Indiani:
+ * il giocatore che gioca questa carta sottopone gli avversari alla possibile perdita di un punto vita,
+ * questi infatti saranno costretti a giocare un bang per difendersi dagli indiani e nel caso non abbiano
+ * carte bang da scartare subiranno danno. Nel caso le sue pallottole si esauriscono potra' usare una birra
+ * in extremis, oppure verra' eliminato.
+ *
+ * @param party party di giocatori
+ * @param nGiocatori numero di giocatori
+ * @param attaccante giocatore che chiama indiani
+ * @param mazzoScarti mazzo di scarti
+ * @param mazzoPesca mazzo di pesca per eventuali bonus
+ * @param nomeFile file di log
+**/
 
 void indiani(Giocatore* party, int* nGiocatori, Giocatore* attaccante, Mazzo* mazzoScarti, Mazzo* mazzoPesca, char* nomeFile)
 {
@@ -165,12 +193,15 @@ void indiani(Giocatore* party, int* nGiocatori, Giocatore* attaccante, Mazzo* ma
     {
         pulisciSchermo();
 
+        // tutti i giocatori tranne l'attaccante dovranno scartare un bang per difendersi
         if(!comparaNomi(attaccante->nome, party[i].nome))
         {
             printf("\n%s ha giocato INDIANI!\n", attaccante->nome);
 
             int idBang = NOP;
-            idBang = trovaCartaInGioco(&party[i], BARILE);
+
+            //trovo la carta bang nella mani del giocatore
+            idBang = trovaCartaMano(&party[i], BANG);
 
             if(idBang != NOP)
             {
@@ -180,9 +211,10 @@ void indiani(Giocatore* party, int* nGiocatori, Giocatore* attaccante, Mazzo* ma
                 strcat(messaggio, " si difende dagli indiani.");
                 salvaLog(messaggio, nomeFile);
 
-                spostaCarta(&party[i].mano, idBang, mazzoScarti, NULL, nomeFile);
+                spostaCarta(&party[i].mano, idBang, mazzoScarti);
                 continuo();
             }
+            //gestisco il caso in cui il giocatore subisca danno
             else
             {
                 printf("\n%s, sei stato colpito!\n", party[i].nome);
@@ -200,7 +232,7 @@ void indiani(Giocatore* party, int* nGiocatori, Giocatore* attaccante, Mazzo* ma
                 {
                     if(birraExtremis(&party[i], nomeFile))
                     {
-                        spostaCarta(&party[i].mano, idBirra, mazzoScarti, NULL, nomeFile);
+                        spostaCarta(&party[i].mano, idBirra, mazzoScarti);
                         birra(&party[i], nomeFile);
                     }
                     else
@@ -218,25 +250,41 @@ void indiani(Giocatore* party, int* nGiocatori, Giocatore* attaccante, Mazzo* ma
     }
 }
 
+/** La procedura Emporio gestisce la giocata della carta Emporio da parte di un giocatore.
+ * Viene creato un array di carte temporaneo in cui inserire carte dal mazzoPesca e ogni giocatore a
+ * turno dovra' scegliere una carta da pescare.
+ *
+ * @param party party di giocatori
+ * @param nGiocatori numero di giocatori
+ * @param attaccante giocatore che gioca emporio
+ * @param mazzoPesca mazzo di pesca
+ * @param mazzoScarti mazzo di scarti da passare se il mazzo pesca e' vuoto
+ * @param nomeFile nome file log
+**/
+
 void emporio(Giocatore* party,int nGiocatori, Giocatore* attaccante, Mazzo* mazzoPesca, Mazzo* mazzoScarti, char* nomeFile)
 {
     printf("\n\n%s ha giocato EMPORIO.\nOgni giocatore deve scegliere una carta.\n", attaccante->nome);
     continuo();
     pulisciSchermo();
 
+    //creazioe struttura mazzo temporanea
     Mazzo temp;
+    //array di carte inizializzato a NULL
     temp.carte = NULL;
     temp.numCarte = 0;
 
+    //passaggio carte dal mazzoPesca al mazzo temporaneo
     for(int i=0; i<nGiocatori; i++)
     {
-        spostaCarta(mazzoPesca, NOP, &temp, mazzoScarti, nomeFile);
+        daiCarta(mazzoPesca, &temp, mazzoScarti, nomeFile);
     }
 
     int scelta = NOP;
 
     stampaCarte(&temp);
 
+    //scelta della carta da parte di chi gioca emporio
     do
     {
         printf("\n%s! Scegli una carta da pescare!\n", attaccante->nome);
@@ -248,7 +296,7 @@ void emporio(Giocatore* party,int nGiocatori, Giocatore* attaccante, Mazzo* mazz
 
     if(scelta != NOP)
     {
-        spostaCarta(&temp, scelta, &attaccante->mano, NULL, nomeFile);
+        spostaCarta(&temp, scelta, &attaccante->mano);
         char messaggio[MEX_DIM] = "";
         strcat(messaggio, attaccante->nome);
         strcat(messaggio, " pesca una carta da Emporio.");
@@ -258,6 +306,7 @@ void emporio(Giocatore* party,int nGiocatori, Giocatore* attaccante, Mazzo* mazz
 
     continuo();
 
+    // scelta della carta da pescare degli altri giocatori
     for(int i=0; i<nGiocatori; i++)
     {
         pulisciSchermo();
@@ -279,7 +328,7 @@ void emporio(Giocatore* party,int nGiocatori, Giocatore* attaccante, Mazzo* mazz
 
             if(scelta != NOP)
             {
-                spostaCarta(&temp, scelta, &party[i].mano, NULL, nomeFile);
+                spostaCarta(&temp, scelta, &party[i].mano);
                 char messaggio[MEX_DIM] = "";
                 strcat(messaggio, party[i].nome);
                 strcat(messaggio, " pesca una carta da Emporio.");
@@ -290,6 +339,4 @@ void emporio(Giocatore* party,int nGiocatori, Giocatore* attaccante, Mazzo* mazz
             continuo();
         }
     }
-
-    free(temp.carte);
 }
